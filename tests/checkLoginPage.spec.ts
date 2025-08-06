c11e0f6c214c6f62d5c58f2c9d8f8dc0285d2a3a
master5100125 <fb.smirnov.u@gmail.com>
2025-08-06 16:12:35 +0300

    fix checkLoginPage test

--- tests/checkLoginPage.spec.ts
+++ tests/checkLoginPage.spec.ts
@@ -1,37 +1,38 @@
 import { test, expect } from '@playwright/test';
 import { HomePage } from '../pages/HomePage';
-// import { UniversalMetods } from '../Utils/UniversalMetods';
 import { LoginPage } from '../pages/LoginPage';
 import { randomUsers } from '../Utils/Credentials';
 import { UniversalMetods } from '../Utils/UniversalMetods';
 
-//треба подумати. Бо на цьому сайті не зберігаються аккаунти в бд, а тілкьи в кукі
+// треба подумати. Бо на цьому сайті не зберігаються аккаунти в бд, а тільки в кукі і тому неможна перевірити логін на сайті
 
-// test.describe('Go to login page', () => {
-//   test.beforeEach(async ({ page }) => {
-//     const homePage = new HomePage(page);
-//     await homePage.goToHomePage();
-//     await homePage.assertTitle('Повнофункціональний фінансовий менеджер');
-//   });
+test.describe('Go to login page', () => {
+  test.beforeEach(async ({ page }) => {
+    const homePage = new HomePage(page);
+    await homePage.goToHomePage();
+    await homePage.assertTitle('Повнофункціональний фінансовий менеджер');
+  });
 
-//   randomUsers.forEach((user) => {
-//     test(`Check authorization with ${user.name}`, async ({ page }) => {
-//       const loginPage = new LoginPage(page);
-//       const unic = new UniversalMetods(page);
-//       const logoValidAuthorization = page.getByTestId('sidebar');
+  randomUsers.forEach((user) => {
+    test(`Check authorization with ${user.name}`, async ({ page }) => {
+      const loginPage = new LoginPage(page);
+      const unic = new UniversalMetods(page);
+      const logoValidAuthorization = page.getByTestId('sidebar');
 
-//       await loginPage.authorization(user.email, user.password);
+      await loginPage.authorization(user.email, user.password);
 
-//       if (user.valid) {
-//         await logoValidAuthorization.waitFor({ state: 'visible', timeout: 10000 });
-//         await unic.safeVisible(logoValidAuthorization);
-//         console.log(user);
-//       } else if (user.valid === false) {
-//         console.log();
-//         await loginPage.errorMessage();
-//       } else {
-//         console.log('Хватить ломати БД');
-//       }
-//     });
-//   });
-// });
+      try {
+        if (user.valid) {
+          await logoValidAuthorization.waitFor({ state: 'visible', timeout: 10000 });
+          await unic.safeVisible(logoValidAuthorization);
+          console.log(user);
+        } else {
+          await loginPage.errorMessage();
+        }
+      } catch (error) {
+        console.log(`Не вдалось залогінитись по користувачеві ${user.name}`);
+        console.error(error);
+      }
+    });
+  });
+});
