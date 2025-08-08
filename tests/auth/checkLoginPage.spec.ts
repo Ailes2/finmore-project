f63def687d668076616ff759e05c2b0d2962e9b5
master5100125 <fb.smirnov.u@gmail.com>
2025-08-08 18:05:03 +0300

    Це можна не дивитись

--- tests/auth/checkLoginPage.spec.ts
+++ tests/auth/checkLoginPage.spec.ts
@@ -6,28 +6,28 @@ import { UniversalMetods } from '../../Utils/UniversalMetods';
 
 // треба подумати. Бо на цьому сайті не зберігаються аккаунти в бд, а тільки в кукі і тому неможна перевірити логін на сайті
 
-test.describe('Go to login page', () => {
-  test.beforeEach(async ({ page }) => {
-    const homePage = new HomePage(page);
-    await homePage.goToHomePage();
-    await homePage.assertTitle('Повнофункціональний фінансовий менеджер');
-  });
+// test.describe('Go to login page', () => {
+//   test.beforeEach(async ({ page }) => {
+//     const homePage = new HomePage(page);
+//     await homePage.goToHomePage();
+//     await homePage.assertTitle('Повнофункціональний фінансовий менеджер');
+//   });
 
-  randomUsers.forEach((user) => {
-    test(`Check authorization with ${user.name}`, async ({ page }) => {
-      const loginPage = new LoginPage(page);
-      const unic = new UniversalMetods(page);
-      const logoValidAuthorization = page.getByTestId('sidebar');
+//   randomUsers.forEach((user) => {
+//     test(`Check authorization with ${user.name}`, async ({ page }) => {
+//       const loginPage = new LoginPage(page);
+//       const unic = new UniversalMetods(page);
+//       const logoValidAuthorization = page.getByTestId('sidebar');
 
-      await loginPage.authorization(user.email, user.password);
+//       await loginPage.authorization(user.email, user.password);
 
-      if (user.valid) {
-        await logoValidAuthorization.waitFor({ state: 'visible', timeout: 10000 });
-        await unic.safeVisible(logoValidAuthorization);
-        console.log(user + ' - успішно залогінився');
-      } else {
-        await loginPage.errorMessage();
-      }
-    });
-  });
-});
+//       if (user.valid) {
+//         await logoValidAuthorization.waitFor({ state: 'visible', timeout: 10000 });
+//         await unic.safeVisible(logoValidAuthorization);
+//         console.log(`${user.name} - успішно залогінився`);
+//       } else {
+//         await loginPage.errorMessage();
+//       }
+//     });
+//   });
+// });
