fed3a081ce57db3cc970f46db93b48eef9199b50
master5100125 <fb.smirnov.u@gmail.com>
2025-08-19 12:25:43 +0300

    add new small test

--- tests/UI/newTransactionWithoutPageObject.ts
+++ tests/UI/newTransactionWithoutPageObject.ts
@@ -27,11 +27,32 @@ test.describe('Create user', () => {
     const buttonNewTransaction = page.getByTestId('add-transaction-button');
     const transactionFormTitle = page.getByTestId('transaction-form-title');
 
+    const expenseButton = page.getByTestId('expense-type-button');
+    const incomeButton = page.getByTestId('income-type-button');
+    const amountInput = page.getByTestId('transaction-amount-input');
+    const transactionCategorySelect = page.getByTestId('transaction-category-select');
+    const descriptionInput = page.getByTestId('transaction-description-input');
+    const dateInput = page.getByTestId('transaction-date-input');
+    const transactionAccountSelect = page.getByTestId('transaction-account-select');
+    const tagInput = page.getByTestId('new-tag-input');
+    const submitButton = page.getByTestId('transaction-form-submit');
+    const cancelButton = page.getByTestId('transaction-form-cancel');
+
     await unic.safeVisible(buttonNewTransaction);
     await unic.Screenshot(testInfo, 'We are loggined in account');
-    await unic.safeVisible(buttonNewTransaction);
     await unic.safeClick(buttonNewTransaction);
     await unic.safeVisible(transactionFormTitle);
     await unic.Screenshot(testInfo, 'Transaction form is opened');
+
+    await unic.safeFill(amountInput, '150');
+    await transactionCategorySelect.selectOption('Транспорт');
+    await unic.safeFill(descriptionInput, 'Якийсь опис транзакції');
+    await unic.safeFill(dateInput, '2025-08-18');
+    await transactionAccountSelect.selectOption('Картка Монобанку');
+    await unic.safeFill(tagInput, 'New tag');
+    await unic.Screenshot(testInfo, 'Transaction form after data entry');
+    await unic.safeClick(submitButton);
+    await unic.safeVisible(buttonNewTransaction);
+    await unic.Screenshot(testInfo, 'Created new transaction');
   });
 });
